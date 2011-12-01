ac7ced5d5138bfedd0ebff68477d28ef0b5436e6
System Administrator <root@Davids-MacBook-Pro.local>
2011-12-01 12:14:56 -0500

    change user type should work.-DW

---
+++ changeType.php
@@ -0,0 +1,19 @@
+<?php
+include "db_connect.php";
+$users=$_POST['users'];
+$type=$_POST['type'];
+if(empty($users)){
+mysqli_close($db);
+header( 'Location: userSearch.php?error=noSel');
+exit;
+}else{
+$N = count($users);
+for($i=0;$i<$N;$i++){
+$query="UPDATE users SET usertype = '$type' WHERE username='$users[$i]'";
+$result = mysqli_query($db, $query);
+}
+mysqli_close($db);	
+header( 'Location: userSearch.php?msg=typeChanged');
+exit;  
+}
+?>
